28586f3cbc82b8da4742d382091248e529d64077
TrueMishka <84371965+TrueMishka@users.noreply.github.com>
2022-11-10 10:36:25 +0300

    Add files via upload

---
+++ array/shopping-list.js
@@ -0,0 +1,16 @@
+/*
+Shopping List
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+*/
+
+const myList = [];
+myList.push(["one", 1]);
+myList.push(["two", 2]);
+myList.push(["three", 3]);
+myList.push(["four", 4]);
+myList.push(["five", 5]);
