a01ecf4a608f32dcab5731e756b3b43e51318295
TrueMishka <84371965+TrueMishka@users.noreply.github.com>
2022-11-11 00:09:31 +0300

    Add files via upload

---
+++ logic/chaining-if-else-statements.js
@@ -0,0 +1,57 @@
+/*
+Logical Order in If Else Statements
+Order is important in if, else if statements.
+
+The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+Take these two functions as an example.
+
+Here's the first:
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+And the second just switches the order of the statements:
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+While these two functions look nearly identical if we pass a number to both we get different outputs.
+
+foo(0)
+bar(0)
+foo(0) will return the string Less than one, and bar(0) will return the string Less than two.
+
+Change the order of logic in the function so that it will return the correct statements in all cases.
+*/
+
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge"; 
+  }
+  // Only change code above this line
+}
+
+testSize(7);
